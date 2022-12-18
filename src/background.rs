735d5eba753018106454cc78989e7ffa1212bc7f
Kevin Wen <kevinwen@Kevins-MBP.attlocal.net>
2022-12-17 20:53:38 -0500

    background cleanup - ryan merged my pr before i cleaned it up LOL

--- src/background.rs
+++ src/background.rs
@@ -14,7 +14,7 @@ pub(crate) fn setup(
     let aspect = 0.5;
 
     // create a new quad mesh. this is what we will apply the texture to
-    let quad_width = 2000.0;
+    let quad_width = 500.0;
     let quad_handle = meshes.add(Mesh::from(shape::Quad::new(Vec2::new(
         quad_width,
         quad_width * aspect,
@@ -29,10 +29,12 @@ pub(crate) fn setup(
     });
 
     // textured quad - normal
-    commands.spawn(PbrBundle {
-        mesh: quad_handle.clone(),
-        material: material_handle,
-        transform: Transform::from_xyz(0.0, 0.0, -300.0),
-        ..default()
-    });
+    commands
+        .spawn(PbrBundle {
+            mesh: quad_handle.clone(),
+            material: material_handle,
+            transform: Transform::from_xyz(0.0, 50.0, -50.0),
+            ..default()
+        });
+
 }
--- src/ui.rs
+++ src/ui.rs
@@ -68,33 +68,5 @@ pub(crate) fn setup_ui_components(mut commands: Commands, asset_server: Res<Asse
             });
           });
 
-        // parent_lv1
-        //   .spawn(NodeBundle {
-        //     style: Style {
-        //       size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-        //       position_type: PositionType::Absolute,
-        //       position: UiRect {
-        //         top: Val::Px(10.0),
-        //         right: Val::Px(10.0),
-        //         ..default()
-        //       },
-        //       justify_content: JustifyContent::Center,
-        //       align_items: AlignItems::Center,
-        //       ..default()
-        //     },
-        //     ..default()
-        //   })
-        //   .with_children(|parent| {
-        //     // bevy logo (image)
-        //     parent.spawn(ImageBundle {
-        //       style: Style {
-        //         size: Size::new(Val::Px(300.0), Val::Auto),
-        //         ..default()
-        //       },
-        //       image: asset_server.load("images/ui_elements/fieldwire-logo-stack.png").into(),
-        //       ..default()
-        //     });
-        //   });
-
     });
 }
